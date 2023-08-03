e1fc58ccd5c81ae802bf6665280146fc5f6d40b0
Nguyễn Kiên <kienlangvai64@gmail.com>
2023-08-03 13:32:06 +0700

    Scan from gallery

--- app/src/main/java/com/kienkk/qrscan/MainActivity.java
+++ app/src/main/java/com/kienkk/qrscan/MainActivity.java
@@ -8,13 +8,17 @@ import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraDevice;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,13 +30,17 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
 
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
+    private static final int REQUEST_IMAGE_PICK = 201;
     private CodeScanner codeScanner;
     private CodeScannerView codeScannerView;
     private TextView data;
+    private ImageView gallery;
     private boolean cameraPermissionGranted = false;
     private SeekBar seekBar;
 
@@ -42,20 +50,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.file);
+        checkPermissionCamera();
 
+        init();
+    }
+
+    private void init(){
+        gallery = findViewById(R.id.gallery);
         codeScannerView = findViewById(R.id.scanner_view);
         data = findViewById(R.id.data);
         seekBar = findViewById(R.id.zoom);
 
         codeScanner = new CodeScanner(this,codeScannerView);
 
-        codeScanner.setScanMode(ScanMode.CONTINUOUS);
-
         codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull Result result) {
-               data.setText(result.getText());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data.setText(result.getText());
+                    }
+                });
+            }
+        });
+
+        codeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
             }
         });
 
@@ -78,10 +101,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        checkPermissionCamera();
-
-        Toast.makeText(this, BarcodeUtils.decodeBitmap(bm).getText(), Toast.LENGTH_SHORT).show();
-
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickImageFromGallery();
+            }
+        });
     }
 
     @Override
@@ -115,5 +140,37 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Vui lòng cho phép truy cập Camera", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_IMAGE_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
+
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            displaySelectedImage(bitmap);
+        }
+    }
+
+    private void displaySelectedImage(Bitmap bm){
+        String text = "";
+        try {
+            text = BarcodeUtils.decodeBitmap(bm).getText();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        data.setText(text);
+        codeScanner.stopPreview();
     }
 }
